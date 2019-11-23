4dbaf23c3bb26b95bba4dc22fc1a1e86c448b08b
Alexey Bass <alexey.bass@gmail.com>
2019-11-23 17:28:34 +0200

    Update dac-setup.ino

--- dac-setup.ino
+++ dac-setup.ino
@@ -1,11 +1,18 @@
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
 
+// ===== CONFIG =====
+uint8_t DAC_ADDRESS = 0x60;
+float DAC_STARTUP_VOLTAGE = 1.800;
+float DAC_SUPPLY_VOLTAGE  = 5.000;
+uint16_t HC_DAC_VALUE_MAX = 4095; // 0x0FFF
+// ===== END OF CONFIG =====
+
 Adafruit_MCP4725 dac;
 
 void setup() {
-  dac.begin(0x60);
-  dac.setVoltage(round((4095 * 1.3) / 3.3), true);
+  dac.begin(DAC_ADDRESS);
+  dac.setVoltage(round((HC_DAC_VALUE_MAX * DAC_STARTUP_VOLTAGE) / DAC_SUPPLY_VOLTAGE), true);
 }
 
 void loop() {
